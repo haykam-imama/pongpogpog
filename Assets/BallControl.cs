c5eae79736441d28dbf84c063b500c232ba19cbf
haykam-imama <18523171@stuents.uii.ac.id>
2020-03-29 02:21:14 -0800

    commit pertama

---
+++ Assets/BallControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    public int speed = 20;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new
+        Vector2 (1,-1)*speed;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
